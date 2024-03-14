03cdaccb00b7e7efb04d61cb412af6b768808208
Colin Unger <lockshaw@lockshaw.net>
2024-03-13 22:34:33 -0700

    Add improved lsp in cuda code and fix up new-branch command

--- flake.nix
+++ flake.nix
@@ -13,10 +13,9 @@
     ];
   };
 
-
   # Nixpkgs / NixOS version to use.
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -27,9 +26,15 @@
         config.allowUnfree = true;
       };
 
+
+      
       # libclangPythonBindings = pkgs.python3Packages.callPackage ./pkgs/libclang.nix { };
     in 
       {
+        packages = {
+          legion = pkgs.callPackage ./.flake/pkgs/legion.nix { };
+          #tokenizers-cpp = pkgs.callPackage ./pkgs/tokenizers-cpp.nix { };
+        };
         # packages = {
         #   libclangPythonBindings = libclangPythonBindings;
         # };
@@ -40,10 +45,14 @@
             export PATH="$HOME/ff/.scripts/:$HOME/ff/.modules/proj/bin/:$PATH"
           '';
           buildInputs = (with pkgs; [
-            clang-tools
+            clang-tools_17
+            llvmPackages_17.clang
+            #clang-tools_17
+            # llvmPackages_17.clang-tools
             cmakeCurses
-            llvmPackages.clang
-            llvmPackages.stdenv
+            gcc10Stdenv
+            gcc10
+            # llvmPackages_14.stdenv
             ccache
             cudatoolkit
             cudaPackages.cuda_nvcc
@@ -53,19 +62,31 @@
             cudaPackages.cuda_cudart
             # cudaPackages.nvidia_driver
             gdb
+            rustc
+            cargo
             zlib
             pkg-config
             bashInteractive
             python3
             gh-markdown-preview
             plantuml
-            libclang
+            # libclang
+            self.packages.${system}.legion
             ruff
             gh
             jq
             compdb
             # self.packages.${system}.libclangPythonBindings
+
+            # flexflow dependencies
+            nlohmann_json
+            spdlog
+            range-v3
+            rapidcheck
+            doctest
+            fmt
           ]) ++ (with pkgs.python3Packages; [
+            gitpython
             ipython
             mypy
             python-lsp-server
